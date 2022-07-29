50fe31b3a76a841aca6502c919a03506194b7b0f
Niharika Harish <Niharika Harish@DESKTOP-9JACVNH>
2022-07-29 11:41:10 +0530

    First Commit

---
+++ ExceptionalTestHandling/src/ExceptionHandling1/ExceptionHandlingDemo2.java
@@ -0,0 +1,27 @@
+package ExceptionHandling1;
+import java.util.Scanner;
+public class ExceptionHandlingDemo2 {
+	static void validate(int age)
+	{
+	if(age<18)
+	throw new ArithmeticException("not valid");
+	else
+	System.out.println("welcome to vote");
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please enter the age");
+		int age=sc.nextInt();
+		try
+		{
+	validate(age);
+		}
+		catch(ArithmeticException ae)
+		{
+			ae.printStackTrace();
+		}
+	System.out.println("rest of the code...");
+	}
+}
