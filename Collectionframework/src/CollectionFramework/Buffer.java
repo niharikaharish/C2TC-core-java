07da39893f6c41a222e4f26b0caaa688095118ac
Niharika Harish <Niharika Harish@DESKTOP-9JACVNH>
2022-08-18 16:59:34 +0530

    First Commit

---
+++ Collectionframework/src/CollectionFramework/Buffer.java
@@ -0,0 +1,16 @@
+package CollectionFramework;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+public class Buffer 
+{
+	private Queue<Integer> clQueue = new ConcurrentLinkedQueue<Integer>();
+	// Retrieving from the queue
+	public void get() {
+		System.out.println("Consumer received - " + clQueue.poll());
+	}
+	// putting in the queue
+	public void put(int i) {
+		clQueue.add(i);
+		System.out.println("Producer Produced - " + i);
+	}
+}
---
+++ Collectionframework/src/CollectionFramework/Student1.java
@@ -0,0 +1,16 @@
+package CollectionFramework;
+import java.util.ArrayList;
+import java.util.Iterator;
+public Student1(int id, String name, int grade)
+		{
+	{
+		this.id = id;
+		this.name = name;
+		this.grade = grade;
+	}
+	public String toString()
+	{
+		String str="Student is ["+id+","+name+","+grade+"]";
+		return str;
+	}
+		}
